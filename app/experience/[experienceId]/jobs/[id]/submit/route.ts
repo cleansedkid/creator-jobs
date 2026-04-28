8b342a7dbd6fd83c039d19a83326cd68b66dc10b
Anton <pratheranton@gmail.com>
2026-04-27 22:13:00 -0700

    fixed i think

--- app/experience/[experienceId]/jobs/[id]/submit/route.ts
+++ app/experience/[experienceId]/jobs/[id]/submit/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { supabaseServer } from "@/lib/supabase/server";
 import { whopsdk } from "@/lib/whop-sdk";
-import { createPayoutSetupToken } from "@/lib/payoutSetupToken";
+import { createPayoutSetupToken, verifyPayoutSetupToken } from "@/lib/payoutSetupToken";
 
 export async function POST(
 	request: NextRequest,
